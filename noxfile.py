a9d2a274bbbea7885639b2635f3532a81d4efee5
Nick <nikolai.in@tuta.io>
2021-11-25 02:55:53 +0300

    :green_heart: Remove mypy from CI
    Because it doesn not work with PTB

--- noxfile.py
+++ noxfile.py
@@ -18,14 +18,13 @@ except ImportError:
     {sys.executable} -m pip install nox-poetry"""
     raise SystemExit(dedent(message)) from None
 
-
 package = "imiit_student_bot"
 python_versions = ["3.10", "3.9", "3.8", "3.7"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
     "safety",
-    "mypy",
+    # "mypy",
     "tests",
     "typeguard",
     "xdoctest",
