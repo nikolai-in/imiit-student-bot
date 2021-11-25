6448b616c1045fd05cfac8e565527ea7f17b853c
Nick <nikolai.in@tuta.io>
2021-11-25 14:22:20 +0300

    :art: Create check_language decorator

--- src/imiit_student_bot/__main__.py
+++ src/imiit_student_bot/__main__.py
@@ -1,6 +1,7 @@
 """Command-line interface."""
 import json
 import logging
+from functools import wraps
 
 import click
 from telegram import InlineKeyboardButton
@@ -16,7 +17,6 @@ from telegram.ext import MessageHandler
 from telegram.ext import PicklePersistence
 from telegram.ext import Updater
 
-
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
@@ -25,6 +25,21 @@ logger = logging.getLogger(__name__)
 
 with open("src/imiit_student_bot/response.json", "r") as read_file:
     response: dict = json.load(read_file)
+
+
+def check_language(func: callable):
+    """Sends typing action while processing func command."""
+
+    @wraps(func)
+    def command_func(update, context, *args, **kwargs):
+        try:
+            lang = context.user_data["Language"]
+        except KeyError:
+            return language_callback(update, context)
+        else:
+            return func(update, context, lang, *args, **kwargs)
+
+    return command_func
 
 
 def language_callback(update: Update, context: CallbackContext) -> None:
@@ -48,93 +63,69 @@ def language_callback(update: Update, context: CallbackContext) -> None:
 def set_language(update: Update, context: CallbackContext) -> None:
     """Set language in user data."""
     query = update.callback_query
+
     query.answer()
 
-    if context.user_data.get("Language-s") == "Start":
-        context.user_data["Language"] = query.data
-        context.user_data["Language-s"] = "Set"
-        start_command(update, context)
-    else:
-        context.user_data["Language"] = query.data
+    context.user_data["Language"] = query.data
+    start_command(update, context)
 
 
-def start_command(update: Update, context: CallbackContext) -> None:
+@check_language
+def start_command(update: Update, context: CallbackContext, lang: str) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
-    try:
-        lang = context.user_data["Language"]
-    except KeyError:
-        context.user_data["Language-s"] = "Start"
-        language_callback(update, context)
-    else:
-        context.bot.send_sticker(
-            chat_id=update.effective_chat.id,
-            sticker=response.get("Sticker").get("Start"),
-        )
-        context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=response.get(lang).get("Start").format(user=user.mention_html()),
-            parse_mode=ParseMode.HTML,
-            reply_markup=ReplyKeyboardMarkup(response.get(lang).get("Keyboard")),
-        )
+    context.bot.send_sticker(
+        chat_id=update.effective_chat.id,
+        sticker=response.get("Sticker").get("Start"),
+    )
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=response.get(lang).get("Start").format(user=user.mention_html()),
+        parse_mode=ParseMode.HTML,
+        reply_markup=ReplyKeyboardMarkup(response.get(lang).get("Keyboard")),
+    )
 
 
-def about_callback(update: Update, context: CallbackContext) -> None:
+@check_language
+def about_callback(update: Update, context: CallbackContext, lang: str) -> None:
     """Send info about the university."""
-    try:
-        lang = context.user_data["Language"]
-    except KeyError:
-        update.message.reply_html(response.get("Error"))
-    else:
-        about_dict = response.get(lang).get("About")
-        keyboard = [
-            [InlineKeyboardButton(button_text, url=link)]
-            for button_text, link in about_dict.items()
-        ]
+    about_dict = response.get(lang).get("About")
+    keyboard = [
+        [InlineKeyboardButton(button_text, url=link)]
+        for button_text, link in about_dict.items()
+    ]
 
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        update.message.reply_sticker(
-            response.get("Sticker").get("About"), reply_markup=reply_markup
-        )
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_sticker(
+        response.get("Sticker").get("About"), reply_markup=reply_markup
+    )
 
 
-def map_callback(update: Update, context: CallbackContext) -> None:
+@check_language
+def map_callback(update: Update, _, lang: str) -> None:
     """Send a map to the university."""
-    try:
-        lang = context.user_data["Language"]
-    except KeyError:
-        update.message.reply_html(response.get("Error"))
-    else:
-        update.message.reply_sticker(response.get("Sticker").get("Map"))
-        update.message.reply_html(text=response.get(lang).get("Map"))
-        update.message.reply_location(
-            latitude=55.7878313846929, longitude=37.60799488989068
-        )
+    update.message.reply_sticker(response.get("Sticker").get("Map"))
+    update.message.reply_html(text=response.get(lang).get("Map"))
+    update.message.reply_location(
+        latitude=55.7878313846929, longitude=37.60799488989068
+    )
 
 
-def timetable_callback(update: Update, context: CallbackContext) -> None:
+@check_language
+def timetable_callback(update: Update, _, lang: str) -> None:
     """Send a map to the university."""
-    try:
-        lang = context.user_data["Language"]
-    except KeyError:
-        update.message.reply_html(response.get("Error"))
-    else:
-        update.message.reply_sticker(response.get("Sticker").get("Timetable"))
-        update.message.reply_html(text=response.get(lang).get("Timetable"))
+    update.message.reply_sticker(response.get("Sticker").get("Timetable"))
+    update.message.reply_html(text=response.get(lang).get("Timetable"))
 
 
-def send_timetable(update: Update, context: CallbackContext) -> None:
+@check_language
+def send_timetable(update: Update, context: CallbackContext, lang: str) -> None:
     """Send a map to the university."""
-    try:
-        lang = context.user_data["Language"]
-    except KeyError:
-        update.message.reply_html(response.get("Error"))
-    else:
-        group = context.match.group(0)
-        logger.info(response.get(lang).get("send_timetable").format(group=group))
-        update.message.reply_html(
-            response.get(lang).get("send_timetable").format(group=group)
-        )
+    group = context.match.group(0)
+    logger.info(response.get(lang).get("send_timetable").format(group=group))
+    update.message.reply_html(
+        response.get(lang).get("send_timetable").format(group=group)
+    )
 
 
 @click.command()
